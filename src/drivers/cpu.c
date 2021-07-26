dbe149f4ab777f300d5f6ec84693bc9db4241442
DARKneuron <animesh.n78@gmail.com>
2021-07-26 15:02:41 +0530

    Drivers modified and added
    CPU.c ,PPU.c added in src/drivers/
    memory.c modified
    dummy test functions added to test the build script

---
+++ src/drivers/cpu.c
@@ -0,0 +1,5 @@
+#include"CPU.h"
+void test_CPU()
+{
+	printf("greetings from CPU\n");
+}
---
+++ src/drivers/memory.c
@@ -0,0 +1,7 @@
+#include<stdio.h>
+#include"memory.h"
+ void test_memory()
+ {
+ 	printf("greetings from memory\n");
+
+ }
